32ba220892af4efc47d27f98aa42c948b8326eab
AFouquet <alex.fdb7@googlemail.com>
2019-07-05 16:45:38 +0200

    my first commit

---
+++ 02_FFMpeg_Converter/FFMpeg_Converter.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+input_array=$(find /Volumes/MrSmith/06_Bash_project/zz_watchFolder/*.mp4)
+#this variable is an array wich has all the paths to our .mp4 files
+echo $input_array #> /Users/alexf/Desktop/echoout.txt
